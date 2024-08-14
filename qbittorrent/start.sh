6fa90c731ac9d1c9cc982bed67979f2ed3881b46
Harry Hart <harry@harryhart.dev>
2024-08-14 19:03:08 +1000

    Allow webui password, certificate, and key to be docker secrets

--- qbittorrent/start.sh
+++ qbittorrent/start.sh
@@ -27,6 +27,13 @@ if [[ ${ENABLE_SSL} == "1" || ${ENABLE_SSL} == "true" || ${ENABLE_SSL} == "yes" 
 		echo "[SYSTEM] If you use Unraid, and get something like a 'ERR_EMPTY_RESPONSE' in your browser, add https:// to the front of the IP, and/or do this:" | ts '%Y-%m-%d %H:%M:%.S'
 		echo "[SYSTEM] Edit this Docker, change the slider in the top right to 'advanced view' and change http to https at the WebUI setting." | ts '%Y-%m-%d %H:%M:%.S'
 	fi
+	# Allow for cert and key to be secrets
+	if [[ -e /run/secrets/WebUICertificate.crt ]]; then
+		ln -s /run/secrets/WebUICertificate.crt /config/qBittorrent/config/WebUICertificate.crt
+	fi
+	if [[ -e /run/secrets/WebUIKey.key ]]; then
+		ln -s /run/secrets/WebUIKey.key /config/qBittorrent/config/WebUIKey.key
+	fi
 	if [ ! -e /config/qBittorrent/config/WebUICertificate.crt ]; then
 		echo "[WARNING] WebUI Certificate is missing, generating a new Certificate and Key" | ts '%Y-%m-%d %H:%M:%.S'
 		openssl req -new -x509 -nodes -out /config/qBittorrent/config/WebUICertificate.crt -keyout /config/qBittorrent/config/WebUIKey.key -subj "/C=NL/ST=localhost/L=localhost/O=/OU=/CN="
@@ -40,13 +47,13 @@ if [[ ${ENABLE_SSL} == "1" || ${ENABLE_SSL} == "true" || ${ENABLE_SSL} == "yes" 
 		echo "[INFO] /config/qBittorrent/config/qBittorrent.conf already has the line WebUICertificate.crt loaded, nothing to do." | ts '%Y-%m-%d %H:%M:%.S'
 	else
 		echo "[WARNING] /config/qBittorrent/config/qBittorrent.conf doesn't have the WebUICertificate.crt loaded. Added it to the config." | ts '%Y-%m-%d %H:%M:%.S'
-		echo 'WebUI\HTTPS\CertificatePath=/config/qBittorrent/config/WebUICertificate.crt' >> "/config/qBittorrent/config/qBittorrent.conf"
+		echo 'WebUI\HTTPS\CertificatePath=/config/qBittorrent/config/WebUICertificate.crt' >>"/config/qBittorrent/config/qBittorrent.conf"
 	fi
 	if grep -Fxq 'WebUI\HTTPS\KeyPath=/config/qBittorrent/config/WebUIKey.key' "/config/qBittorrent/config/qBittorrent.conf"; then
 		echo "[INFO] /config/qBittorrent/config/qBittorrent.conf already has the line WebUIKey.key loaded, nothing to do." | ts '%Y-%m-%d %H:%M:%.S'
 	else
 		echo "[WARNING] /config/qBittorrent/config/qBittorrent.conf doesn't have the WebUIKey.key loaded. Added it to the config." | ts '%Y-%m-%d %H:%M:%.S'
-		echo 'WebUI\HTTPS\KeyPath=/config/qBittorrent/config/WebUIKey.key' >> "/config/qBittorrent/config/qBittorrent.conf"
+		echo 'WebUI\HTTPS\KeyPath=/config/qBittorrent/config/WebUIKey.key' >>"/config/qBittorrent/config/qBittorrent.conf"
 	fi
 	if grep -xq 'WebUI\\HTTPS\\Enabled=true\|WebUI\\HTTPS\\Enabled=false' "/config/qBittorrent/config/qBittorrent.conf"; then
 		if grep -xq 'WebUI\\HTTPS\\Enabled=false' "/config/qBittorrent/config/qBittorrent.conf"; then
@@ -57,7 +64,7 @@ if [[ ${ENABLE_SSL} == "1" || ${ENABLE_SSL} == "true" || ${ENABLE_SSL} == "yes" 
 		fi
 	else
 		echo "[WARNING] /config/qBittorrent/config/qBittorrent.conf doesn't have the WebUI\HTTPS\Enabled loaded. Added it to the config." | ts '%Y-%m-%d %H:%M:%.S'
-		echo 'WebUI\HTTPS\Enabled=true' >> "/config/qBittorrent/config/qBittorrent.conf"
+		echo 'WebUI\HTTPS\Enabled=true' >>"/config/qBittorrent/config/qBittorrent.conf"
 	fi
 elif [[ ${ENABLE_SSL} == "0" || ${ENABLE_SSL} == "false" || ${ENABLE_SSL} == "no" ]]; then
 	echo "[WARNING] ENABLE_SSL is set to '${ENABLE_SSL}', SSL is not enabled. This could cause issues with logging if other apps use the same Cookie name (SID)." | ts '%Y-%m-%d %H:%M:%.S'
@@ -70,17 +77,17 @@ fi
 # Check qbtUser, change UID/GID if they don't match the environment variables.
 qbtUID=$(id -u qbtUser)
 qbtGID=$(id -g qbtUser)
-if [[ ${PUID} !=  $qbtUID || ${PGID} != $qbtGID ]]; then
+if [[ ${PUID} != $qbtUID || ${PGID} != $qbtGID ]]; then
 	usermod -u ${PUID} -g ${PGID} qbtUser
 fi
 
 # Start qBittorrent
 echo "[INFO] Starting qBittorrent daemon..." | ts '%Y-%m-%d %H:%M:%.S'
 chmod -R 755 /config/qBittorrent
-nohup /bin/bash /entrypoint.sh > /dev/null 2>&1 &
+nohup /bin/bash /entrypoint.sh >/dev/null 2>&1 &
 
 # wait for the entrypoint.sh script to finish and grab the qbittorrent pid
-while ! pgrep -f "qbittorrent-nox" > /dev/null; do
+while ! pgrep -f "qbittorrent-nox" >/dev/null; do
 	sleep 0.5
 done
 qbittorrentpid=$(pgrep -f "qbittorrent-nox")
@@ -135,20 +142,20 @@ if [ -e /proc/$qbittorrentpid ]; then
 	while true; do
 		# First wait for the health check interval.
 		sleep ${INTERVAL}
-		
+
 		# Confirm the process is still running, start it back up if it's not.
-		if ! ps -p $qbittorrentpid > /dev/null; then
+		if ! ps -p $qbittorrentpid >/dev/null; then
 			echo "[ERROR] qBittorrent daemon is not running. Restarting..." | ts '%Y-%m-%d %H:%M:%.S'
-			nohup /bin/bash /entrypoint.sh > /dev/null 2>&1 &
+			nohup /bin/bash /entrypoint.sh >/dev/null 2>&1 &
 
 			# wait for the entrypoint.sh script to finish and grab the qbittorrent pid
-			while ! pgrep -f "qbittorrent-nox" > /dev/null; do
+			while ! pgrep -f "qbittorrent-nox" >/dev/null; do
 				sleep 0.5
 			done
 			qbittorrentpid=$(pgrep -f "qbittorrent-nox")
 		fi
 		# Ping uses both exit codes 1 and 2. Exit code 2 cannot be used for docker health checks, therefore we use this script to catch error code 2
-		ping -c ${HEALTH_CHECK_AMOUNT} $HOST > /dev/null 2>&1
+		ping -c ${HEALTH_CHECK_AMOUNT} $HOST >/dev/null 2>&1
 		STATUS=$?
 		if [[ "${STATUS}" -ne 0 ]]; then
 			echo "[ERROR] Network is possibly down." | ts '%Y-%m-%d %H:%M:%.S'
@@ -161,19 +168,22 @@ if [ -e /proc/$qbittorrentpid ]; then
 		if [[ ${HEALTH_CHECK_SILENT,,} == "0" || ${HEALTH_CHECK_SILENT,,} == "false" || ${HEALTH_CHECK_SILENT,,} == "no" ]]; then
 			echo "[INFO] Network is up" | ts '%Y-%m-%d %H:%M:%.S'
 		fi
-		
+
 		# Check the NAT port forward and update qBittorrent config if there is a change.
-		if [[ $ENABLEPROTONVPNPORTFWD -eq 1  ]] ; then
+		if [[ $ENABLEPROTONVPNPORTFWD -eq 1 ]]; then
+			if [[ -e /run/secrets/webui_pass ]]; then
+				WEBUI_PASS=$(cat /run/secrets/webui_pass)
+			fi
 			loginData="username=$WEBUI_USER&password=$WEBUI_PASS"
-			cookie=$(curl -i --silent --header "Referer: $WEBUI_URL" --data $loginData $WEBUI_URL/api/v2/auth/login | grep "set-cookie" | awk '/set-cookie:/ {print $2}' | sed 's/;//') > /dev/null 2>&1
+			cookie=$(curl -i --silent --header "Referer: $WEBUI_URL" --data $loginData $WEBUI_URL/api/v2/auth/login | grep "set-cookie" | awk '/set-cookie:/ {print $2}' | sed 's/;//') >/dev/null 2>&1
 			if [[ $cookie ]]; then
-				setPort=$(curl --silent $WEBUI_URL/api/v2/app/preferences --cookie $cookie | jq '.listen_port') > /dev/null 2>&1
+				setPort=$(curl --silent $WEBUI_URL/api/v2/app/preferences --cookie $cookie | jq '.listen_port') >/dev/null 2>&1
 				currentPort=$(natpmpc -a 1 0 udp 60 -g 10.2.0.1 | grep "public port" | awk '/Mapped public port/ {print $4}')
-				if [[ $setPort -ne $currentPort ]] ; then		
+				if [[ $setPort -ne $currentPort ]]; then
 					portData="json={\"listen_port\":$currentPort}"
-					curl -i --silent --data $portData $WEBUI_URL/api/v2/app/setPreferences --cookie $cookie > /dev/null 2>&1
+					curl -i --silent --data $portData $WEBUI_URL/api/v2/app/setPreferences --cookie $cookie >/dev/null 2>&1
 				fi
-				curl --silent -X 'POST' "$WEBUI_URL/api/v2/auth/logout" -H 'accept: */*' -d '' --cookie $cookie > /dev/null 2>&1
+				curl --silent -X 'POST' "$WEBUI_URL/api/v2/auth/logout" -H 'accept: */*' -d '' --cookie $cookie >/dev/null 2>&1
 			else
 				echo "[WARNING] Unable to log into the web UI." | ts '%Y-%m-%d %H:%M:%.S'
 			fi
